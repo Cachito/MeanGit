237968b3feb38cc2282ebc8b154ccfde15345931
Cachito <luis.carro@gmail.com>
2017-03-26 10:35:51 -0300

    commit 21/03

---
+++ cacho.js
@@ -0,0 +1,28 @@
+// mean clase 2 js
+
+var app = angular.module('CachoApp', ['ngMaterial', 'ngMdIcons']);
+
+app.controller('cachoCtrl', ['$scope', '$mdBottomSheet', '$mdSidenav', '$mdDialog',
+    function($scope, $mdBottomSheet, $mdSidenav, $mdDialog){
+        $scope.toggleSidenav = function(menuId){
+            $mdSidenav(menuId).toggle();
+        };
+
+    $scope.menu =[
+        {
+            link:'#',
+            title: 'Inicio',
+            icon: 'dashboard'
+        },
+        {
+            link:'#',
+            title: 'Rockers',
+            icon: 'group'
+        },
+        {
+            link:'#',
+            title: 'Mensajes',
+            icon: 'message'
+        }
+    ];
+}]);
